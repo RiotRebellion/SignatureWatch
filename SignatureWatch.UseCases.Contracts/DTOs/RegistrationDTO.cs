624cfdce735d5e5ac83f60a72899c1f4db6b7335
RiotRebellion <misha.dzhurmiy@gmail.com>
2022-06-01 12:55:47 +0800

    add Contracts

---
+++ SignatureWatch.UseCases.Contracts/DTOs/RegistrationDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SignatureWatch.UseCases.Contracts.DTOs
+{
+    public class RegistrationDTO
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ SignatureWatch.UseCases.Contracts/Profiles/RegistrationProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using SignatureWatch.Domain.Entities;
+using SignatureWatch.UseCases.Contracts.DTOs;
+
+namespace SignatureWatch.UseCases.Contracts.Profiles
+{
+    public class RegistrationProfile : Profile
+    {
+        public RegistrationProfile()
+        {
+            CreateMap<User, RegistrationDTO>()
+                .ForMember(dest =>
+                    dest.Username,
+                    opt => opt.MapFrom(src => src.Username))
+                .ForMember(dest =>
+                    dest.Password,
+                    opt => opt.MapFrom(src => src.Password))
+                .ForMember(dest =>
+                    dest.Email,
+                    opt => opt.MapFrom(src => src.Email));
+        }
+    }
+}
