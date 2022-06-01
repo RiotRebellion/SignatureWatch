37ff6b11284e3808a2f775da8da7d0ab60b478cc
RiotRebellion <misha.dzhurmiy@gmail.com>
2022-06-01 12:53:52 +0800

    Add EntityConfiguration

---
+++ SignatureWatch.Infrastructure.Persistence/Configurations/Base/EntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SignatureWatch.Domain.Entities.Base;
+
+namespace SignatureWatch.Infrastructure.Persistence.Configurations.Base
+{
+    internal abstract class EntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
+        where TEntity : Entity
+    {
+        public void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).ValueGeneratedOnAdd();
+
+            ConfigureOtherProperties(builder);
+        }
+
+        protected abstract void ConfigureOtherProperties(EntityTypeBuilder<TEntity> builder);
+    }
+}
---
+++ SignatureWatch.Infrastructure.Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SignatureWatch.Domain.Entities;
+using SignatureWatch.Infrastructure.Persistence.Configurations.Base;
+
+namespace SignatureWatch.Infrastructure.Persistence.Configurations
+{
+    internal class UserConfiguration : EntityConfiguration<User>
+    {
+        protected override void ConfigureOtherProperties(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(p => p.Username).IsRequired();
+            builder.Property(p => p.Password).IsRequired();
+            builder.Property(p => p.Email).IsRequired();
+        }
+    }
+}
