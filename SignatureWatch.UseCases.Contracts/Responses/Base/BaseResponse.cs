5dc29706d7df9e24fef58655022d63ae541d0888
Михаил Джурмий <misha.dzhurmiy@gmail.com>
2022-06-27 16:17:42 +0800

    separate base realization of response

---
+++ SignatureWatch.UseCases.Contracts/Responses/Base/BaseResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace SignatureWatch.UseCases.Contracts.Responses.Base
+{
+    public abstract class BaseResponse
+    {
+        public bool IsSuccess { get; set; }
+
+        public string[] Errors { get; set; }
+    }
+}
