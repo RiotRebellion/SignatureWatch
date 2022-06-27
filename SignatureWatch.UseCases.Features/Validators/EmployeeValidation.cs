0a7e0be1ffe371fc71af2fb173fae0357338af06
Михаил Джурмий <misha.dzhurmiy@gmail.com>
2022-06-27 16:21:22 +0800

    Add EmployeeValidation

--- SignatureWatch.UseCases.Features/Validators/EmployeeValidation.cs
+++ SignatureWatch.UseCases.Features/Validators/EmployeeValidation.cs
@@ -1,13 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using SignatureWatch.UseCases.Contracts.ViewModels;
 
 namespace SignatureWatch.UseCases.Features.Validators
 {
-    public class EmployeeValidation
+    public class EmployeeValidation :AbstractValidator<EmployeeViewModel>
     {
+        public EmployeeValidation()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("Заполните имя сотрудника");
 
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Заполните фамилию сотрудника");
+
+            RuleFor(x => x.Department)
+                .NotEmpty().WithMessage("Заполните отдел сотрудника");
+        }
     }
 }
