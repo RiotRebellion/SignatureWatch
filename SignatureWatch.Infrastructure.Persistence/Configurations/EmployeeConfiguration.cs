3bf6d5daedecbc9a54784b73ab0178b697cd0b9b
RiotRebellion <misha.dzhurmiy@gmail.com>
2022-07-01 04:14:03 +0800

    (feat) Add configurations to Signature and Employee

---
+++ SignatureWatch.Infrastructure.Persistence/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SignatureWatch.Domain.Entities;
+using SignatureWatch.Infrastructure.Persistence.Configurations.Base;
+
+namespace SignatureWatch.Infrastructure.Persistence.Configurations
+{
+    internal class EmployeeConfiguration : EntityConfiguration<Employee>
+    {
+        protected override void ConfigureOtherProperties(EntityTypeBuilder<Employee> builder)
+        {
+        }
+    }
+}
---
+++ SignatureWatch.Infrastructure.Persistence/Configurations/SignatureConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SignatureWatch.Domain.Entities;
+using SignatureWatch.Infrastructure.Persistence.Configurations.Base;
+
+namespace SignatureWatch.Infrastructure.Persistence.Configurations
+{
+    internal class SignatureConfiguration : EntityConfiguration<Signature>
+    {
+        protected override void ConfigureOtherProperties(EntityTypeBuilder<Signature> builder)
+        {
+            builder.Property(p => p.Owner).IsRequired();
+        }
+    }
+}
