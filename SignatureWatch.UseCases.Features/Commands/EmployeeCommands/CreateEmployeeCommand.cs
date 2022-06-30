a097d45ca7471b23518243d7ad027374703fbec6
RiotRebellion <misha.dzhurmiy@gmail.com>
2022-07-01 01:39:31 +0800

    (fix) restore after merge

---
+++ SignatureWatch.UseCases.Features/Commands/EmployeeCommands/CreateEmployeeCommand.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using SignatureWatch.UseCases.Contracts.DTO;
+using SignatureWatch.UseCases.Gateways;
+
+namespace SignatureWatch.UseCases.Features.Commands.EmployeeCommands
+{
+    public class CreateEmployeeCommand : IRequest<int>
+    {
+        public EmployeeDTO SignatureDTO;
+
+        public class CreateEmployeeCommandHandler : IRequestHandler<CreateEmployeeCommand, int>
+        {
+            private readonly IMapper _mapper;
+            private readonly IDbContext _dbContext;
+
+            public CreateEmployeeCommandHandler(IDbContext dbContext, IMapper mapper)
+            {
+                _dbContext = dbContext;
+                _mapper = mapper;
+            }
+
+            public async Task<int> Handle(CreateEmployeeCommand command, CancellationToken cancellationToken)
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ SignatureWatch.UseCases.Features/Commands/SignatureCommands/CreateSignatureCommand.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using SignatureWatch.UseCases.Contracts.DTO;
+using SignatureWatch.UseCases.Gateways;
+
+namespace SignatureWatch.UseCases.Features.Commands.SignatureCommands
+{
+    public class CreateSignatureCommand : IRequest<int>
+    {
+        public SignatureDTO SignatureDTO;
+
+        public class CreateSignatureCommandHandler : IRequestHandler<CreateSignatureCommand, int>
+        {
+            private readonly IMapper _mapper;
+            private readonly IDbContext _dbContext;
+
+            public CreateSignatureCommandHandler(IDbContext dbContext, IMapper mapper)
+            {
+                _dbContext = dbContext;
+                _mapper = mapper;
+            }
+
+            public async Task<int> Handle(CreateSignatureCommand command, CancellationToken cancellationToken)
+            {
+                return 0;
+            }
+        }
+    }
+}
