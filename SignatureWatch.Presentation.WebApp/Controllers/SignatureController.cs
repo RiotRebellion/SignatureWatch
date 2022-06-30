dabe7b437d0d0da8918a1d056364ffdd02e0f2c9
RiotRebellion <misha.dzhurmiy@gmail.com>
2022-07-01 04:16:56 +0800

    (Feat) Modify SignatureConfroller (create, getall)

--- SignatureWatch.Presentation.WebApp/Controllers/SignatureController.cs
+++ SignatureWatch.Presentation.WebApp/Controllers/SignatureController.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using SignatureWatch.Presentation.WebApp.Controllers.Base;
+using SignatureWatch.UseCases.Contracts.DTO;
+using SignatureWatch.UseCases.Features.Commands.SignatureCommands;
 using SignatureWatch.UseCases.Features.Queries.SignatureQueries;
 
 namespace SignatureWatch.Presentation.WebApp.Controllers
@@ -10,9 +12,17 @@ namespace SignatureWatch.Presentation.WebApp.Controllers
     public class SignatureController : ApiController
     {
         [HttpGet]
-        public async Task<IActionResult> GetSignatures()
+        [Route("GetAllSignatures")]
+        public async Task<IActionResult> GetAllSignatures()
         {
             return Ok(await Mediator.Send(new GetAllSignaturesQuery()));
+        }
+
+        [HttpPost]
+        [Route("CreateSignature")]
+        public async Task<IActionResult> CreateSignature([FromBody] SignatureDTO signatureDTO)
+        {
+            return Ok(await Mediator.Send(new CreateSignatureCommand { SignatureDTO = signatureDTO}));
         }
     }
 }
