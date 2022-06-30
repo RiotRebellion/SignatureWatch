adfb9ce13043748f5a8223dd2e22bac4bdd45018
RiotRebellion <misha.dzhurmiy@gmail.com>
2022-07-01 04:13:38 +0800

    (fix) Add ownerId to Signature entity

--- SignatureWatch.Domain.Entities/Signature.cs
+++ SignatureWatch.Domain.Entities/Signature.cs
@@ -16,6 +16,8 @@ namespace SignatureWatch.Domain.Entities
 
         public SignatureType SignatureType { get; set; }
 
+        public Guid OwnerId { get; set; }
+
         public Employee Owner { get; set; }
     }
 }
