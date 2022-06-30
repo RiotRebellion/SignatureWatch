4bcba91b9d9a47183ff96193110d4284153cf8ee
RiotRebellion <misha.dzhurmiy@gmail.com>
2022-07-01 00:59:25 +0800

    (feat) add EmployeeValidation

--- SignatureWatch.UseCases.Features/Validators/EmployeeValidation.cs
+++ SignatureWatch.UseCases.Features/Validators/EmployeeValidation.cs
@@ -1,13 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using SignatureWatch.UseCases.Contracts.DTO;
 
 namespace SignatureWatch.UseCases.Features.Validators
 {
-    public class EmployeeValidation
+    public class EmployeeValidation : AbstractValidator<EmployeeDTO>
     {
+        public EmployeeValidation()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name should not be empty");
 
+            RuleFor(x => x.Department)
+                .NotEmpty().WithMessage("Department should not be empty");
+
+            RuleFor(x => x.EmployeeStatus)
+                .NotEmpty().WithMessage("EmployeeStatus should not be empty");
+        }
     }
 }
