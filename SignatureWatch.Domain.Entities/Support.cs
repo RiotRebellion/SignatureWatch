f839dd703e5c209a5f615abd65b601f5c0826ff3
Михаил Джурмий <misha.dzhurmiy@gmail.com>
2022-10-10 10:24:50 +0800

    (feat) add name to support

--- SignatureWatch.Domain.Entities/Support.cs
+++ SignatureWatch.Domain.Entities/Support.cs
@@ -6,6 +6,8 @@ namespace SignatureWatch.Domain.Entities
     {
         public string ActivationCode { get; set; }
 
+        public string Name { get; set; }
+
         public DateTime ExpirationDate { get; set; }
 
         public string? PhoneNumber { get; set; }
--- SignatureWatch.Infrastructure.Persistence/Migrations/20221010022401_AddNameToSupport.Designer.cs
+++ SignatureWatch.Infrastructure.Persistence/Migrations/20221010022401_AddNameToSupport.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SignatureWatch.Infrastructure.Persistence.Contexts;
@@ -11,9 +12,10 @@ using SignatureWatch.Infrastructure.Persistence.Contexts;
 namespace SignatureWatch.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221010022401_AddNameToSupport")]
+    partial class AddNameToSupport
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
